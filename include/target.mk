ee0dde80b76a4a4ef728fed3fd2ed2abcd4f367e
KFERMercer <KFER.Mercer@gmail.com>
2020-02-18 03:50:17 +0800

    target: split samba from default

--- include/target.mk
+++ include/target.mk
@@ -21,7 +21,7 @@ luci-app-pptp-server luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramf
 luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol \
 ddns-scripts_aliyun ddns-scripts_dnspod
 # For nas targets
-DEFAULT_PACKAGES.nas:=fdisk lsblk mdadm automount autosamba luci-app-usb-printer
+DEFAULT_PACKAGES.nas:=fdisk lsblk mdadm automount luci-app-usb-printer
 # For router targets
 DEFAULT_PACKAGES.router:=dnsmasq-full iptables ppp ppp-mod-pppoe firewall kmod-ipt-offload kmod-tcp-bbr
 DEFAULT_PACKAGES.bootloader:=
